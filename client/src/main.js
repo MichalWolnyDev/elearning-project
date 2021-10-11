1158964062db7c34a17c1f40be463be36033a8cb
MichalWolnyDev <mwolny98@interia.pl>
2021-10-12 00:02:15 +0200

    init project

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import vuex from "vuex";
+import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuex,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
