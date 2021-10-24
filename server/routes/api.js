44e93b821a59f4f7d039f18c57273706eb3b169a
MichalWolnyDev <mwolny98@interia.pl>
2021-10-24 12:58:09 +0200

    backend login v1

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,89 @@
-const express = require('express');
-const path = require('path');
+const User = require('../models/user');
+const {auth} = require('../middlewares/auth');
 
-const router = express.Router();
+const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+  res.status(200).send(`Welcome to login , sign-up api`);
+})
+
+// adding new user (sign-up route)
+router.post('/api/register', function (req, res) {
+  // taking a user
+  const newuser = new User(req.body);
+  console.log(newuser);
+
+
+  if (newuser.password != newuser.confirmPassword) return res.status(400).json({ message: "Hasła do siebie nie pasują" });
+
+  User.findOne({ email: newuser.email }, function (err, user) {
+    if (user) return res.status(400).json({ auth: false, message: "Podany email jest już zarejestrowany" });
+
+    newuser.save((err, doc) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({ success: false });
+      }
+      res.status(200).json({
+        succes: true,
+        user: doc
+      });
+    });
+  });
 });
 
-module.exports = router;
+// login user
+router.post('/api/login', function(req,res){
+  let token = req.cookies.auth;
+
+  User.findByToken(token,(err,user)=>{
+      if(err) return  res(err);
+      if(user) return res.status(400).json({
+          error: true,
+          message: "Jesteś już zalogowany"
+      });
+  
+      else{
+          User.findOne({'email':req.body.email}, function(err,user) {
+              if(!user) return res.json({isAuth: false, message: 'Nie znaleziono takiego adresu e-mail'});
+      
+              user.comparePassword(req.body.password, (err,isMatch) => {
+                  if(!isMatch) return res.json({isAuth: false, message: "Hasła nie są identyczne"});
+      
+              user.generateToken((err,user) => {
+                  if(err) return res.status(400).send(err);
+                  res.cookie('auth',user.token).json({
+                      isAuth : true,
+                      id : user._id,
+                      email : user.email
+                  });
+              });    
+          });
+        });
+      }
+  });
+});
+
+// get logged in user
+router.get('/api/profile',auth,function(req,res){
+  console.log(res);
+  res.json({
+      isAuth: true,
+      id: req.user._id,
+      email: req.user.email,
+      name: req.user.firstName + " " + req.user.lastName
+      
+  })
+});
+
+//logout user
+router.get('/api/logout',auth,function(req,res){
+
+  req.user.deleteToken(req.token,(err,user)=>{
+      if(err) return res.status(400).send(err);
+      res.sendStatus(200);
+  });
+
+}); 
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -2,10 +2,8 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const User = require('./models/user');
-const {auth} = require('./middlewares/auth');
+
 
 // parse env variables
 require('dotenv').config();
@@ -21,17 +19,11 @@ const app = express();
 // Configure middlewares
 app.use(cors());
 app.use(express.json());
-app.use(bodyparser.urlencoded({extended : false}));
-app.use(bodyparser.json());
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
 app.use(cookieParser())
 
 // app.set('view engine', 'html');
-
-// Static folder
-// app.use(express.static(__dirname + '/views/'));
-
-// Defining route middleware
-// app.use('/api', require('./routes/api'));
 
 // database connection
 mongoose.Promise=global.Promise;
@@ -40,9 +32,10 @@ mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopo
     console.log("database is connected");
 });
 
-app.get('/', function(req,res){
-    res.status(200).send(`Welcome to login , sign-up api`);
-});
+
+// Defining route middleware
+app.use('/', require('./routes/api'));
+
 
 // Listening to port
 
