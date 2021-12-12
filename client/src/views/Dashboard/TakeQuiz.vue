01a3be5f1af0a76aafda99f7f79d119e228f08ad
MichalWolnyDev <mwolny98@interia.pl>
2021-12-12 15:15:42 +0100

    takequiz temp

--- client/src/views/Dashboard/TakeQuiz.vue
+++ client/src/views/Dashboard/TakeQuiz.vue
@@ -17,18 +17,29 @@
         >
 
           <h2 class="question__title">Pytanie nr. {{ id + 1 }}: {{ question.text }}</h2>
-          <div class="answer__row" v-for="(answer, id) in question.answers" :key="id">
+          <div class="answer__row" v-for="(answer, answerId) in question.answers" :key="answerId">
             <div>
-                <strong>{{ toLetters(id+1) }}:</strong>
+                <strong>{{ toLetters(answerId+1) }}:</strong>
             {{ answer.text }}
             </div>
             <div>
-                <v-checkbox>
+              {{ id }} {{ answerId }}
+               <!-- v-model="userAnswers[id][answerId].answer" -->
+               <!-- {{userAnswers[id][answerId]}} -->
+                <v-checkbox @change="checkAnswer(id, answerId)">
                     
                 </v-checkbox>
             </div>
           </div>
         </v-card>
+      </v-row>
+    </v-container>
+
+    <v-container>
+      <v-row class="d-flex justify-center pa-3">
+        <v-btn color="success">
+          Zapisz
+        </v-btn>
       </v-row>
     </v-container>
   </div>
@@ -41,6 +52,12 @@ export default {
     ...mapGetters(["getSingleQuiz"]),
     
   },
+  data: () => ({
+    userAnswers: [],
+    checkedAnswer: [],
+    quizLength: null,
+    // answerObj: 
+  }),
   methods: {
     ...mapActions(["fetchSingleQuiz"]),
     toLetters(num) {
@@ -49,11 +66,39 @@ export default {
         out = mod ? String.fromCharCode(64 + mod) : (--pow, "Z");
       return pow ? this.toLetters(pow) + out : out;
     },
+    checkAnswer(questionid, answerid){
+      console.log(questionid)
+      console.log(answerid)
+      
+      // console.log(this.userAnswers[questionid])
+      // this.userAnswers[questionid].answerTrigger = null
+      // this.userAnswers[questionid].answerTrigger = answerid
+      // this.userAnswers[questionid][answerid].answer = true
+      // console.log(this.userAnswers)
+      this.userAnswers[questionid][answerid].answer = !this.userAnswers[questionid][answerid].answer 
+      console.log("answer clicked");
+      // if (!this.userAnswers[questionid]) this.userAnswers[questionid] = [] // if the current question has no answers mapped yet set an empty array 
+      // this.userAnswers[questionid] = this.checkedAnswers[questionid]; // set the answer to whatever value it has currently (true, false)
+    }
   },
   mounted() {},
   created() {
     this.fetchSingleQuiz(this.$route.params.id);
   },
+  // watch: {
+  //   'getSingleQuiz': {
+  //     handler: function(){
+  //       var tmp = [{answer: false, test: "dupa"}, {answer: false}, {answer: false}, {answer: false}];
+  //       this.quizLength = this.getSingleQuiz.questions.length;
+
+  //       if(this.quizLength > 0){
+  //         for(var i = 0; i < this.quizLength; i++){
+  //           this.userAnswers[i] = tmp;
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 };
 </script>
 <style lang="scss" scoped>
